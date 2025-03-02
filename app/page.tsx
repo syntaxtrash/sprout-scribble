f403cd1868949ab6b779c6d2ff4105191f36a6cc
Aaron Aco <aaronacodev@gmail.com>
2025-03-02 08:25:31 +0800

    clean up to start

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,8 @@
-import PostButton from "@/components/post-button";
-import createPost from "@/server/actions/create-posts";
-import getPosts from "@/server/actions/get-posts";
 
 export default async function Home() {
-  const posts = await getPosts();
   return (
     <main>
-      {posts.map((post) => {
-        return (
-          <div key={post.id}>
-            <h1>{post.title}</h1>
-          </div>
-        );
-      })}
-      <form action={createPost}>
-        <input type="text" name="title" placeholder="title"/>
-        <PostButton/>
-      </form>
+      <h1>Homepage</h1>
     </main>
   );
 }
--- server/actions/get-posts.ts
+++
@@ -1,9 +0,0 @@
-"use server"
-
-import { db } from "@/server"
-
-export default async function getPosts() {
-    const posts = await db.query.posts.findMany()
-
-    return posts ?? []
-}
