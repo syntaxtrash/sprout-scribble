227d25730b8dbea8da4031bb586b0bb311c44aef
Aaron Aco <aaronacodev@gmail.com>
2025-03-02 09:34:09 +0800

    Setup shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import { Button } from "@/components/ui/button";
 
 export default async function Home() {
   return (
     <main>
       <h1>Homepage</h1>
+      <Button>Click me</Button>
     </main>
   );
 }
